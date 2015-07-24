92e81eba1712f78ec4468b13e58a043f376e6455
Rubeenaks <rubeenaks@gmail.com>
2015-07-24 11:11:22 +0530

    Rearranged the flow of program

--- src/ScrabbleBestWordFinder.java
+++ src/ScrabbleBestWordFinder.java
@@ -4,11 +4,12 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class ScrabbleBestWordFinder {
-	
+
 	final int rackSize = 7;
 	String rack;
 	ArrayList<String> rackList = new ArrayList<String>();
@@ -17,21 +18,22 @@ public class ScrabbleBestWordFinder {
 	final int[] letterScores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
 			25, 26};
 	int[] rackLetterCount = new int[26];
-	
+
 	ScrabbleBestWordFinder (String rack, String fName) {
 		this.rack = sortWord(rack.toUpperCase().trim());
 		createRackSpace(rack);
 		populateRackLetterCounts ();
 		this.dictionaryFileName = fName;
 	}
-	
-	private void populateRackLetterCounts () {
-		char[] letters = rack.toCharArray();
-		for (char letter : letters) {
-			rackLetterCount[letter - 'A']++;
-		}
+
+	public static void main(String args[]) {
+
+		String rack = "OUR ";
+		String fileName = "C:\\Users\\test\\Desktop\\sowpods.txt";
+		ScrabbleBestWordFinder scrabbleBestWordFinder = new ScrabbleBestWordFinder(rack, fileName);
+		System.out.println(scrabbleBestWordFinder.findBestWord());
 	}
-	
+
 	private int createRackSpace (String rack) {
 		if (! rack.contains(" ")) {
 			rackList.add(sortWord(rack.toUpperCase()));
@@ -40,61 +42,30 @@ public class ScrabbleBestWordFinder {
 		char alpha = 'A';
 		int index = rack.indexOf(" ");
 		char[] word = rack.toCharArray();
-		
+
 		while (Arrays.asList(word).contains(" ") || alpha != 'Z') {
 			word[index] = alpha;
 			alpha ++;
 			createRackSpace(new String(word));
 		}
-		
+
 		word[index] = 'Z';
 		rackList.add(new String(word).toUpperCase());
 		return 0;
 	}
-	
-	private boolean isLetterInRack (char letter, int[] letterCount) {
-		
-		if (letterCount[letter - 'A'] > 0) {
-			letterCount[letter - 'A']--;
-			return true;
-		}
-		return false;
-	}
-	
-	private int computeWordScore(String word) {
-		int[] letterScores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-				25, 26};
-		int[] rackLetterCountCopy = Arrays.copyOf(this.rackLetterCount, this.rackLetterCount.length);
-		int wordScore = 0;
-		for (char letter : word.toCharArray()) {
-			if ( isLetterInRack(letter, rackLetterCountCopy)) {
-				wordScore += letterScores[letter - 'A'];
-			}
-		}
-		
-		return wordScore;
-	}
-	
-	private void addWordsToDictionary(int score,String word){
-		
-		if (dictionary.containsKey(score))
-		{
-			dictionary.get(score).add(word);	
-		} else {
-			
-			//To be checked
-			ArrayList<String> wordList= new ArrayList<String>();
-			wordList.add(word);
-			dictionary.put(score, wordList);
+
+	private void populateRackLetterCounts () {
+		char[] letters = rack.toCharArray();
+		for (char letter : letters) {
+			rackLetterCount[letter - 'A']++;
 		}
 	}
-	
-	private String sortWord(String word) {
-		char[] letters = word.toCharArray();
-		Arrays.sort(letters);
-		return new String(letters);
+
+	public String findBestWord() {
+		buildDictionary();
+		return extractValidWords();
 	}
-	
+
 	private void buildDictionary()
 	{
 		File file = new File(this.dictionaryFileName);
@@ -106,7 +77,7 @@ public class ScrabbleBestWordFinder {
 			while (sc.hasNextLine()) {
 				String word = sc.next();
 				if (word.length() <= rackSize) {
-						addWordsToDictionary(computeWordScore(word),word);
+					addWordsToDictionary(computeWordScore(word),word);
 				}
 			}
 			sc.close();
@@ -115,11 +86,48 @@ public class ScrabbleBestWordFinder {
 			e.printStackTrace();
 		}
 	}
-	
+
+	private int computeWordScore(String word) {
+		int[] letterScores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+				25, 26};
+		int[] rackLetterCountCopy = Arrays.copyOf(this.rackLetterCount, this.rackLetterCount.length);
+		int wordScore = 0;
+		for (char letter : word.toCharArray()) {
+			if ( isLetterInRack(letter, rackLetterCountCopy)) {
+				wordScore += letterScores[letter - 'A'];
+			}
+		}
+
+		return wordScore;
+	}
+
+	private boolean isLetterInRack (char letter, int[] letterCount) {
+
+		if (letterCount[letter - 'A'] > 0) {
+			letterCount[letter - 'A']--;
+			return true;
+		}
+		return false;
+	}
+
+	private void addWordsToDictionary(int score,String word){
+
+		if (dictionary.containsKey(score))
+		{
+			dictionary.get(score).add(word);	
+		} else {
+
+			//To be checked
+			ArrayList<String> wordList= new ArrayList<String>();
+			wordList.add(word);
+			dictionary.put(score, wordList);
+		}
+	}
+
 	private String extractValidWords() {
 
 		String validWords = "";
-		for (Map.Entry<Integer, ArrayList<String>> entry : dictionary.entrySet()) {
+		for (Entry<Integer, ArrayList<String>> entry : dictionary.entrySet()) {
 			ArrayList<String> wordsList = entry.getValue();
 			for (String word : wordsList) {
 				if (isWordValid(word)) {
@@ -132,9 +140,9 @@ public class ScrabbleBestWordFinder {
 		}
 		return validWords.trim();
 	}
-	
+
 	private boolean isWordValid(String word) {
-		
+
 		for (String rack : rackList) {
 			if (rack.contains(sortWord(word))) {
 				return true;
@@ -142,17 +150,11 @@ public class ScrabbleBestWordFinder {
 		}
 		return false;
 	}
-	
-	public String findBestWord() {
-		buildDictionary();
-		return extractValidWords();
+
+	private String sortWord(String word) {
+		char[] letters = word.toCharArray();
+		Arrays.sort(letters);
+		return new String(letters);
 	}
-	
-	public static void main(String args[]) {
-		
-		String rack = "OURTA ";
-		String fileName = "C:\\Users\\test\\Desktop\\sowpods.txt";
-		ScrabbleBestWordFinder scrabbleBestWordFinder = new ScrabbleBestWordFinder(rack, fileName);
-		System.out.println(scrabbleBestWordFinder.findBestWord());
-	}
+
 }
